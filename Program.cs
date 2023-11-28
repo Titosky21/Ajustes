29e15f76b6e1b9c7767b8006f316adfacecdd289
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-27 22:22:43 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,14 +5,44 @@ namespace AjustesProcesos{
     public class Program{
         public static void Main(string[] args){
         string rutaArchivo = "Procesos.txt";    
+        string rutaArchivo2 = "Procesos1.txt";
+        string file = " ";
         string[] vectorNombreProceso = new string[100];  
         int[] vectorTiempoLlegada = new int[100];
         int[] vectorTiempoEjecucion = new int[100];
         int[] vectorPesoProceso = new int[100];
+        
+        bool flag = true;
+        int opcion = 0;
+        while(flag){
+            System.Console.WriteLine($"Procesos.txt funciona para Primer Ajuste");
+            System.Console.WriteLine($"Procesos1.txt funciona para Peor Ajuste");
+            System.Console.WriteLine("¿Que archivo desea abrir?:");
+            System.Console.WriteLine("1) Procesos.txt 2) Procesos1.txt");
+            System.Console.Write("Opcion:");
+            
+            
+            opcion = System.Convert.ToInt32(System.Console.ReadLine());
+            if(opcion != 1 || opcion != 2){
+                flag = true;
+            }
+            if(opcion == 1){
+                file = rutaArchivo;
+                flag = false;
+            }
+            if(opcion == 2){
+                file = rutaArchivo2;
+                flag = false;
+            }
+            if(opcion == 3){
+                System.Console.WriteLine("Cerrando Programa...");
+                flag = false;
+            }
+        
             try{
 
                 int indice = 0;  
-                using (StreamReader sr = new StreamReader(rutaArchivo)){
+                using (StreamReader sr = new StreamReader(file)){
                     string linea;
 
                     while ((linea = sr.ReadLine()) != null){
@@ -28,13 +58,39 @@ namespace AjustesProcesos{
                 }
 
                 for (int i = 0; i < indice; i++){
-                    Console.WriteLine($"Proceso: {vectorNombreProceso[i]}, Llegada: {vectorTiempoLlegada[i]}, Ejecución: {vectorTiempoEjecucion[i]}, Peso: {vectorPesoProceso[i]}");
+                    Console.WriteLine($"Proceso: {vectorNombreProceso[i]}, Llegada: {vectorTiempoLlegada[i]}, Ejecución: {vectorTiempoEjecucion[i]}, Peso: {vectorPesoProceso[i]}kb");
                 }
             }
             catch (Exception e){
                 Console.WriteLine($"Error al leer el archivo: {e.Message}");
             }
 
+            if(opcion == 1){
+                PrimerAjuste PrimerA = new PrimerAjuste(vectorNombreProceso, vectorTiempoLlegada, vectorTiempoEjecucion, vectorPesoProceso);
+                PrimerA.GenerarAjuste();
+
+                System.Console.WriteLine("1) Procesos.txt 2) Procesos1.txt");
+                System.Console.Write("Probar Otro Archivo:");
+                opcion = System.Convert.ToInt32(System.Console.ReadLine());
+                if(opcion == 3){
+                    System.Console.WriteLine("Cerrando Programa...");
+                    flag = false;
+                }
+            }
+            if(opcion == 2){
+                PeorAjuste PeorA = new PeorAjuste(vectorNombreProceso, vectorTiempoLlegada, vectorTiempoEjecucion, vectorPesoProceso);
+                PeorA.GenerarAjuste();
+
+                System.Console.WriteLine("1) Procesos.txt 2) Procesos1.txt");
+                System.Console.Write("Probar Otro Archivo:");
+                opcion = System.Convert.ToInt32(System.Console.ReadLine());
+                if(opcion == 3){
+                    System.Console.WriteLine("Cerrando Programa...");
+                    flag = false;
+                }
+            }
+        }
         }
     }
 }
+
